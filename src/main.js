a7888e44f1857e8bf83e7a9b58f67a389c7932d9
luowen <374285130@qq.com>
2025-12-05 22:55:54 +0800

    main.js创建了vue对象把App这个Vue组件挂载到了index.html的app div
    浏览器先加载index.html,然后执行main.js此时dom中已经包含app所以可以直接挂载

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 关闭 Vue 在生产模式下的“提示信息”（即启动时的 "You are running Vue in development mode..." 警告），避免用户混淆。
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//h 是 createElement 的简写（hyperscript）；
+var vue = new Vue({
+    render: h => h(App),
+});
+
+// new Vue({
+//     render(createElement) {
+//         return createElement(App)
+//     }
+// })
+
+vue.$mount('#app')
