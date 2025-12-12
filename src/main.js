19a677daf77b16a0f49144cb59239b10ad016a47
luowen <374285130@qq.com>
2025-12-13 01:24:49 +0800

    从 element-ui 包中导入整个 Element UI 组件库。import ElementUI from 'element-ui';
    import 'element-ui/lib/theme-chalk/index.css';引入 Element UI 的默认样式文件（基于 theme-chalk 主题）。
    Vue.use(ElementUI) 的作用是 将 Element UI 作为 Vue 插件安装到你的 Vue 应用中，从而让所有 Element UI 提供的组件（如 <el-button>、<el-input> 等）和指令（如 v-loading）可以在整个项目中全局使用。Vue.use() 是 Vue.js 提供的一个 API，用于安装插件。ElementUI 对象内部定义了 install 方法。这个 install 方法做了以下事情：
                                                                                                                                                                                                                把所有组件（Button、Input、Table 等）全局注册为 Vue 组件；
                                                                                                                                                                                                                注册自定义指令（如 v-loading）；
                                                                                                                                                                                                                设置一些全局配置（如语言、尺寸等，默认值）。

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // 关闭 Vue 在生产模式下的“提示信息”（即启动时的 "You are running Vue in development mode..." 警告），避免用户混淆。
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 //h 是 createElement 的简写（hyperscript）；
 var vue = new Vue({
     render: h => h(App),
 });
+
 
 // new Vue({
 //     render(createElement) {
